b202842a113d17721e71eb2ab68a42aa4839c8d7
Gustavo Barcelos <gustavo.barcelos@bmgmoney.com>
2019-02-04 16:16:41 -0200

    mudando a estrutura do projeto

---
+++ cobranca/src/main/java/br/com/oak/aworks/cobranca/service/CadastroTituloService.java
@@ -0,0 +1,54 @@
+package br.com.oak.aworks.cobranca.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import br.com.oak.aworks.cobranca.model.StatusTitulo;
+import br.com.oak.aworks.cobranca.model.Titulo;
+import br.com.oak.aworks.cobranca.repository.Titulos;
+import br.com.oak.aworks.cobranca.repository.filter.TituloFilter;
+
+@Service
+public class CadastroTituloService {
+
+	@Autowired
+	private Titulos titulos;
+
+	public List<Titulo> findAll() {
+		return titulos.findAll();
+	}
+
+	public void salvar(Titulo titulo) {
+
+		try {
+
+			titulos.save(titulo);
+
+		} catch (DataIntegrityViolationException e) {
+			throw new IllegalArgumentException("Formato de data inválido");
+		}
+	}
+
+	public void excluir(Long codigo) {
+		titulos.delete(codigo);
+	}
+	
+	public String receber(Long codigo) {
+
+		Titulo titulo = titulos.findOne(codigo);
+
+		titulo.setStatus(StatusTitulo.RECEBIDO);
+		
+		titulos.save(titulo);
+
+		return StatusTitulo.RECEBIDO.getDescricao();
+	}
+
+	public List<Titulo> filtrar(final TituloFilter filtro) {
+		String descricao = filtro.getDescricao() == null ? "%" : filtro.getDescricao();
+		return titulos.findByDescricaoContaining(descricao);
+	}
+}
