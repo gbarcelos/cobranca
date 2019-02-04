5cc42c9941e42d32c737f6da6ed1eb374154fd34
Gustavo Barcelos <gustavo.barcelos@bmgmoney.com>
2019-02-04 16:13:37 -0200

    removendo diretorios desnecessarios

--- 02_conectando-ao-mysql/cobranca/src/main/java/br/com/oak/aworks/cobranca/repository/Titulos.java
+++
@@ -1,9 +0,0 @@
-package br.com.oak.aworks.cobranca.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.oak.aworks.cobranca.model.Titulo;
-
-public interface Titulos extends JpaRepository<Titulo, Long> {
-
-}
